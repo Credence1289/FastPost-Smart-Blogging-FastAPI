3d2546827bfe79cc8ebac677960e6cb54357d46a
Vinayak Dewoolkar <dewoolkar.vinayak@gmail.com>
2026-04-01 00:48:24 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -15,9 +15,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://fast-post-lovat.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
